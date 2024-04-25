c7b8f78aad4a52b25ad27e6473c719ae2f4786eb
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-04-25 19:29:02 +0800

    refactor: windows shell update.

--- gen.cmd
+++ gen.cmd
@@ -2,11 +2,21 @@
 setlocal
 
 rem Define array elements
-set "PROTO_NAMES=auth conversation errinfo friend group msg msggateway push rtc sdkws third user statistics"
+set "PROTO_NAMES=auth conversation errinfo friend group msg msggateway push rtc sdkws third user statistics wrapperspb"
 
 rem Loop through each element in the array
 for %%i in (%PROTO_NAMES%) do (
-  protoc --go_out=plugins=grpc:./%%i --go_opt=module=github.com/openimsdk/protocol/%%i %%i/%%i.proto
+    protoc --go_out=plugins=grpc:./%%i --go_opt=module=github.com/openimsdk/protocol/%%i %%i/%%i.proto
+    if ERRORLEVEL 1 (
+        echo error processing %%i.proto
+        exit /b %ERRORLEVEL%
+    )
 )
+
+rem Replace "omitempty" in *.pb.go files with UTF-8 encoding
+for /r %%f in (*.pb.go) do (
+    powershell -Command "(Get-Content -Path '%%f' -Encoding UTF8) -replace ',omitempty\"`"', '\"`"' | Set-Content -Path '%%f' -Encoding UTF8"
+)
+
 
 endlocal
