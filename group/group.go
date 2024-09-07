c58b470a19ee247d7b3100ab4bf3a5c4e5526001
Monet Lee <monet_lee@163.com>
2024-09-07 18:43:46 +0800

    fix

--- group/group.go
+++ group/group.go
@@ -17,6 +17,8 @@ package group
 import (
 	"errors"
 	"fmt"
+
+	"github.com/openimsdk/protocol/constant"
 )
 
 func (x *CreateGroupReq) Check() error {
@@ -30,7 +32,10 @@ func (x *CreateGroupReq) Check() error {
 		return errors.New("GroupType is invalid")
 	}
 	if x.OwnerUserID == "" {
-		return errors.New("ownerUserID")
+		return errors.New("ownerUserID is empty")
+	}
+	if len(x.MemberUserIDs) > constant.ParamMaxLength {
+		return errors.New("too many MemberUserIDs, need to be less than 1000")
 	}
 	return nil
 }
@@ -160,6 +165,10 @@ func (x *KickGroupMemberReq) Check() error {
 	if x.KickedUserIDs == nil {
 		return errors.New("kickUserIDs is empty")
 	}
+
+	if len(x.KickedUserIDs) > constant.ParamMaxLength {
+		return errors.New("too many KickedUserIDs, need to be less than 1000")
+	}
 	return nil
 }
 
@@ -184,6 +193,11 @@ func (x *InviteUserToGroupReq) Check() error {
 	if x.InvitedUserIDs == nil {
 		return errors.New("invitedUserIDs is empty")
 	}
+
+	if len(x.InvitedUserIDs) > constant.ParamMaxLength {
+		return errors.New("too many InvitedUserIDs, need to be less than 1000")
+	}
+
 	return nil
 }
 
@@ -286,7 +300,11 @@ func (x *SetGroupMemberInfo) Check() error {
 
 func (x *SetGroupMemberInfoReq) Check() error {
 	if x.Members == nil {
-		return errors.New("Members is empty")
+		return errors.New("members is empty")
+	}
+
+	if len(x.Members) > constant.ParamMaxLength {
+		return errors.New("too many Members, need to be less than 1000")
 	}
 	return nil
 }
@@ -294,6 +312,10 @@ func (x *SetGroupMemberInfoReq) Check() error {
 func (x *GetGroupAbstractInfoReq) Check() error {
 	if x.GroupIDs == nil {
 		return errors.New("GroupID is empty")
+	}
+
+	if len(x.GroupIDs) > constant.ParamMaxLength {
+		return errors.New("too many GroupIDs, need to be less than 1000")
 	}
 	return nil
 }
@@ -304,6 +326,10 @@ func (x *GetUserInGroupMembersReq) Check() error {
 	}
 	if x.UserID == "" {
 		return errors.New("userID is empty")
+	}
+
+	if len(x.GroupIDs) > constant.ParamMaxLength {
+		return errors.New("too many GroupIDs, need to be less than 1000")
 	}
 	return nil
 }
@@ -347,6 +373,10 @@ func (x *GetGroupUsersReqApplicationListReq) Check() error {
 	}
 	if x.UserIDs == nil {
 		return errors.New("UserID is empty")
+	}
+
+	if len(x.UserIDs) > constant.ParamMaxLength {
+		return errors.New("too many UserIDs, need to be less than 1000")
 	}
 	return nil
 }
