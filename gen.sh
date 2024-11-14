787b7ad55cc74485280dbea7b42f643411a9d47b
Monet Lee <monet_lee@163.com>
2024-11-14 14:19:56 +0800

    fix: solve grpc generate. (#160)

--- gen.sh
+++ gen.sh
@@ -17,7 +17,7 @@ PROTO_NAMES=(
 )
 
 for name in "${PROTO_NAMES[@]}"; do
-    protoc --go_out=./${name} --go_opt=module=github.com/openimsdk/protocol/${name} ${name}/${name}.proto
+ protoc --go_out=./${name} --go_opt=module=github.com/openimsdk/protocol/${name} ${name}/${name}.proto
   if [ $? -ne 0 ]; then
       echo "error processing ${name}.proto (go_out)"
       exit $?
@@ -26,13 +26,13 @@ done
 
 # generate go-grpc
 
-# for name in "${PROTO_NAMES[@]}"; do
-#  protoc --go-grpc_out=./${name} --go-grpc_opt=module=github.com/openimsdk/protocol/${name} ${name}/${name}.proto
-#   if [ $? -ne 0 ]; then
-#       echo "error processing ${name}.proto (go-grpc_out)"
-#       exit $?
-#   fi
-# done
+for name in "${PROTO_NAMES[@]}"; do
+ protoc --go-grpc_out=./${name} --go-grpc_opt=module=github.com/openimsdk/protocol/${name} ${name}/${name}.proto
+  if [ $? -ne 0 ]; then
+      echo "error processing ${name}.proto (go-grpc_out)"
+      exit $?
+  fi
+done
 
 if [ "$(uname -s)" == "Darwin" ]; then
     find . -type f -name '*.pb.go' -exec sed -i '' 's/,omitempty"`/\"\`/g' {} +
--- gen_openmeeting.sh
+++ gen_openmeeting.sh
@@ -14,13 +14,13 @@ done
 
 # generate go-grpc
 
-# for name in "${PROTO_NAMES[@]}"; do
-#  protoc --go-grpc_out=./openmeeting/${name} --go_opt=module=github.com/openimsdk/protocol/openmeeting/${name} openmeeting/${name}/${name}.proto
-#   if [ $? -ne 0 ]; then
-#       echo "error processing ${name}.proto (go-grpc_out)"
-#       exit $?
-#   fi
-# done
+for name in "${PROTO_NAMES[@]}"; do
+ protoc --go-grpc_out=./openmeeting/${name} --go_opt=module=github.com/openimsdk/protocol/openmeeting/${name} openmeeting/${name}/${name}.proto
+  if [ $? -ne 0 ]; then
+      echo "error processing ${name}.proto (go-grpc_out)"
+      exit $?
+  fi
+done
 
 if [ "$(uname -s)" == "Darwin" ]; then
     find . -type f -name '*.pb.go' -exec sed -i '' 's/,omitempty"`/\"\`/g' {} +
